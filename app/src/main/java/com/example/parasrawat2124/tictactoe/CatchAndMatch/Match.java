32a045251cc88dac699ecde8c2b4b7fd36837939
Paras Rawat <parasmyname@gmail.com>
2019-02-09 12:26:21 +0530

    Safe Delete Dragging Functionality

--- app/src/main/java/com/example/parasrawat2124/tictactoe/CatchAndMatch/Match.java
+++ app/src/main/java/com/example/parasrawat2124/tictactoe/CatchAndMatch/Match.java
@@ -1,24 +1,14 @@
 package com.example.parasrawat2124.tictactoe.CatchAndMatch;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.DragEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.parasrawat2124.tictactoe.ModelClass.GamerProfile;
-import com.example.parasrawat2124.tictactoe.ModelClass.MyDragEventListener;
-import com.example.parasrawat2124.tictactoe.MyDragShadowBuilder;
 import com.example.parasrawat2124.tictactoe.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -37,6 +27,7 @@ public class Match extends AppCompatActivity {
     ImageView imageView11,imageView12,imageView13,imageView21,imageView22,imageView23,imageView31,imageView32,imageView33;
     String player2;
     String player1;
+    //todo Player 1 is always the challenger player, player 2 is always the challenged player
     CircleImageView gamer1,gamer2;
     TextView gamer1name,gamer2name;
     @Override
@@ -60,82 +51,11 @@ public class Match extends AppCompatActivity {
         imageView33=findViewById(R.id.text33);
         crosss.setTag(IMAGEVIEW_TAG);
         zero.setTag(IMAGEVIEW_TAG);
-        zero.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                String i=v.getTag().toString();
-
-                ClipData.Item item=new ClipData.Item(i);
-
-                ClipData dragData = new ClipData(
-                        v.getTag().toString(),
-                        new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN },
-                        item);
-                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(zero);
-                v.startDrag(dragData,myShadow,v,0);
-
-                return true;
-            }
-        });
-        crosss.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                String i=v.getTag().toString();
-
-                ClipData.Item item=new ClipData.Item(i);
-
-                ClipData dragData = new ClipData(
-                        v.getTag().toString(),
-                        new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN },
-                        item);
-                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(crosss);
-                v.startDrag(dragData,myShadow,v,0);
-                return true;
-            }
-        });
-        imageView11.setOnDragListener(dragListener);
-        imageView12.setOnDragListener(dragListener);
-        imageView13.setOnDragListener(dragListener);
-        imageView21.setOnDragListener(dragListener);
-        imageView22.setOnDragListener(dragListener);
-        imageView23.setOnDragListener(dragListener);
-        imageView31.setOnDragListener(dragListener);
-        imageView32.setOnDragListener(dragListener);
-        imageView33.setOnDragListener(dragListener);
 
         fillData();
     }
 
-    View.OnDragListener dragListener=new View.OnDragListener() {
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
 
-            //takes all the information of the view that is dragged
-            int dragevent=event.getAction();
-            switch (dragevent){
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    return true;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    return true;
-                case DragEvent.ACTION_DROP:
-                    final View view= (View) event.getLocalState();
-                    Log.d(TAG, "onDrag: "+view.getId()+"  "+R.id.cross);
-                    if(view.getId()==R.id.cross) {
-                        v.setBackgroundResource(R.drawable.cross);
-                        //todo Check for the game is over or not
-                    }
-                    else if(view.getId()==R.id.zero){
-                        v.setBackgroundResource(R.drawable.zero);
-                        //todo check if the game is over or not
-
-                }
-                        return true;
-            }
-            return true;
-
-            //todo D
-        }
-    };
 
     String getSharedPreferences(){
         SharedPreferences sharedPreferences=getSharedPreferences("Gamers",MODE_PRIVATE);
