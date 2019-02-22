11fc92402b6a97beff68bee93b9a21a383c53704
Paras Rawat <parasmyname@gmail.com>
2019-02-22 05:59:28 +0530

    Logic for 1 view

--- app/src/main/java/com/example/parasrawat2124/tictactoe/CatchAndMatch/Match.java
+++ app/src/main/java/com/example/parasrawat2124/tictactoe/CatchAndMatch/Match.java
@@ -9,9 +9,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.parasrawat2124.tictactoe.ModelClass.GamerProfile;
+import com.example.parasrawat2124.tictactoe.ModelClass.Response;
 import com.example.parasrawat2124.tictactoe.ModelClass.Responses;
 import com.example.parasrawat2124.tictactoe.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -116,6 +118,172 @@ public class Match extends AppCompatActivity {
                 }
             });
         }
+
+        //Updater Login
+        DatabaseReference updater=FirebaseDatabase.getInstance().getReference("MatchProcess");
+        updater.child(getGame()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: "+"Updater is called");
+                Responses responses=dataSnapshot.getValue(Responses.class);
+                if(!responses.getBlock11().equals("empty")){
+                    if(responses.getBlock11().equals("zero")){
+                        imageView11.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView11.setImageResource(R.drawable.cross);
+                    }
+                }
+
+                if(!responses.getBlock12().equals("empty")){
+                    if(responses.getBlock12().equals("zero")){
+                        imageView12.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView12.setImageResource(R.drawable.cross);
+                    }
+                }
+
+                if(!responses.getBlock13().equals("empty")){
+                    if(responses.getBlock13().equals("zero")){
+                        imageView13.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView13.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock21().equals("empty")){
+                    if(responses.getBlock21().equals("zero")){
+                        imageView21.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView21.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock22().equals("empty")){
+                    if(responses.getBlock22().equals("zero")){
+                        imageView22.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView22.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock23().equals("empty")){
+                    if(responses.getBlock23().equals("zero")){
+                        imageView23.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView23.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock31().equals("empty")){
+                    if(responses.getBlock31().equals("zero")){
+                        imageView31.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView31.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock32().equals("empty")){
+                    if(responses.getBlock32().equals("zero")){
+                        imageView32.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView32.setImageResource(R.drawable.cross);
+                    }
+                }
+                if(!responses.getBlock33().equals("empty")){
+                    if(responses.getBlock33().equals("zero")){
+                        imageView33.setImageResource(R.drawable.zero);
+                    }
+                    else {
+                        imageView33.setImageResource(R.drawable.cross);
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+        final DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference("MatchProcess");
+        //On First Start..
+        imageView11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child(getGame()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        gettingreadycardview.setVisibility(View.VISIBLE);
+                        Log.d(TAG, "onDataChange: "+dataSnapshot);
+                        Responses responses=dataSnapshot.getValue(Responses.class);
+                        if(getGameStarter().equals(responses.getTurn())){
+
+                            if(responses.getBlock11().equals("empty")){
+                                //todo allow the player to make the change
+                                if(getGameStarter().equals("player1")){
+                                    imageView11.setImageResource(R.drawable.cross);
+                                    Responses responses1=new Responses("cross","empty","empty","empty","empty","empty","empty","empty","empty","false","true","","player2");
+                                    DatabaseReference databaseReference1=FirebaseDatabase.getInstance().getReference("MatchProcess");
+                                    databaseReference1.child(getGame()).setValue(responses1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(getApplicationContext(), "Successfully Pushed, Waiting reply from other player", Toast.LENGTH_SHORT).show();
+                                                gettingreadycardview.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+
+                                }
+                                else if(getGameStarter().equals("player2")){
+                                    imageView11.setImageResource(R.drawable.zero);
+                                    Responses responses1=new Responses("zero","empty","empty","empty","empty","empty","empty","empty","empty","false","true","","player1");
+                                    DatabaseReference databaseReference1=FirebaseDatabase.getInstance().getReference("MatchProcess");
+                                    databaseReference1.child(getGame()).setValue(responses1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(getApplicationContext(), "Successfully Pushed, Waiting reply from other player", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+
+
+                                    });
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(),"I dont know what to do",Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"Not a valid response, already filled ",Toast.LENGTH_LONG).show();
+                                gettingreadycardview.setVisibility(View.GONE);
+                            }
+
+                        }
+                        else {
+                            //todo not the players turn; so wait for the reply of other player
+                            gettingreadycardview.setVisibility(View.GONE);
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 
 
